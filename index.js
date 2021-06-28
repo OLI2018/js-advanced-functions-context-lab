2673788792616d10da8472b09e6b306e06eabba8
Oleg <roxvemm2014@gmail.com>
2021-06-27 23:57:56 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,62 @@
 /* Your Code Here */
+function createEmployeeRecord(employeeArr)
+{
+    const obj={
+        firstName:employeeArr[0],
+        familyName:employeeArr[1],
+        title:employeeArr[2],
+        payPerHour:employeeArr[3],
+        timeInEvents:[],
+        timeOutEvents:[]
+    }
+    return obj;
+}
+let createEmployeeRecords= function(arrOfArr){
+return arrOfArr.map(x=>createEmployeeRecord(x));
+}
+//dateStamp "YYYY-MM-DD HHMM"
+let createTimeInEvent= function(dateStamp){
+    let spl=dateStamp.split(" "); //
+    this.timeInEvents.push({
+        type:"TimeIn",
+        hour:parseInt(spl[1],10),
+        date:spl[0]
+    })
+    return this
+}
+let createTimeOutEvent= function(dateStamp){
+    let spl=dateStamp.split(" ");
+    this.timeOutEvents.push({
+        type:"TimeOut",
+        hour:parseInt(spl[1],10),
+        date:spl[0]
+    })
+    return this
+}
+//dateForm ="YYYY-MM-DD"
+let hoursWorkedOnDate= function(dateForm){
+ const inEvent=this.timeInEvents.find(x=>x.date===dateForm);
+ const outEvent=this.timeOutEvents.find(x=>x.date===dateForm);
+ const totalHoursWorked=(outEvent.hour-inEvent.hour)/100
+ return totalHoursWorked;
+}
 
+let wagesEarnedOnDate= function(dateForm)
+{
+    return this.payPerHour*hoursWorkedOnDate.call(this,dateForm);
+}
+
+let findEmployeeByFirstName=function(srcArray,firstName){
+    return srcArray.find(x=>x.firstName===firstName);
+   }
+   
+let calculatePayroll= function(arr){
+    return arr.reduce((accum,x)=>accum+allWagesFor.call(x),0);
+   }
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
  sneaky bug that we'll cover in the next few lessons!
-
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
